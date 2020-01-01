c8961980c8e3c7fbee61734cd9743ea343105a06
abbbhucho <abcbhucho@gmail.com>
2020-01-01 22:14:05 +0530

    fixed bugs

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -84,12 +84,13 @@ if [[ $COMPOSER_IS_INSTALLED -ne 0 ]]; then
 		echo ''
 		echo ">>> Verfiying Composer setup data integrity "
 		echo ''
-		$HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
+		HASH="$(wget -q -O - https://composer.github.io/installer.sig)"
 		php -r "if (hash_file('SHA384', 'composer-setup.php') === '$HASH') { echo 'Installer Verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 		echo ''
 		echo ">>> Installing Composer"
 		echo ''
 		sudo php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+		rm composer-setup.php
 	fi
 else
     echo ">>> Updating Composer"
