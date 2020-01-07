daaa8ccae6834645cc7bd9fe3fe5bc122e0a3b2f
abbbhucho <abcbhucho@gmail.com>
2020-01-07 13:19:50 +0530

    updated laravel installation

--- setup_laravel.sh
+++ setup_laravel.sh
@@ -103,4 +103,84 @@ else
 fi
 # After successful installation of composer
 composer -V
+echo ''
+echo '>>> Checking for apache/nginx server'
+echo ''
+# Test if Apache or Nginx is installed
+nginx -v > /dev/null 2>&1
+NGINX_IS_INSTALLED=$?
 
+apache2 -v > /dev/null 2>&1
+APACHE_IS_INSTALLED=$?
+if [[ $APACHE_IS_INSTALLED -eq 0 ]]; then
+	if ! pidof apache2 > /dev/null
+	then
+		echo  "Apache web server  is down, Trying auto-restart"
+
+		# web server down, restart the server
+		sudo /etc/init.d/apache2 restart > /dev/null
+		sleep 10
+
+		#checking if apache restarted or not
+		if pidof apache2 > /dev/null
+		then
+			echo "Apache restarted successfully"
+		else
+			echo "Restart Failed, try restarting manually"
+		fi
+	else
+		echo "Apache web server is running"	
+	fi
+else																																																																																																																																																																																																																																																																																																
+	if [ -e /var/run/nginx.pid ]; then echo "nginx is running";
+	else	echo 'nginx is not running or installed! Please use command "service nginx start" to start the server or "sudo apt install nginx" to install it' & exit 0; 
+
+	fi
+fi	
+#----------------------------------------------------------------------------------------------------------------------------------
+echo ''
+echo '>>> Installing Laravel'
+echo ''
+echo 'Enter the name of the Laravel project'
+read project
+
+# Test if Vagrant is installed
+vagrant --version > /dev/null 2>&1
+VAGRANT_IS_INSTALLED=$?
+
+if [[ $VAGRANT_IS_INSTALLED -eq 0 ]]; then
+    echo 'Vagrant is installed, checking for laravel VagrantBox'
+	# Test if Server IP is set in Vagrantfile
+	echo 'Do you want to continue with Laravel vagrant box? y or n'
+	read con
+	if [ $con == 'y' ]; then
+		[[ -z "$1" ]] && { printf "!!! IP address not set. Check the Vagrantfile.\n    Installing Laravel aborted!\n"; exit 0; }
+	fi
+fi
+# Test if Laravel Homestead is installed
+homestead --version > /dev/null 2>&1
+HOMESTEAD_IS_INSTALLED=$?
+# Check if Laravel root is set. If not set use default
+if [[ $HOMESTEAD_IS_INSTALLED -eq 0 ]]; then
+	echo 'Laravel/Homestead is installed successfully'
+    laravel_root_folder="/vagrant/laravel"
+else
+    laravel_root_folder="$project"
+fi
+echo "laravel root folder : $PWD/$laravel_root_folder"
+laravel_public_folder="$laravel_root_folder/public"
+# Create Laravel
+	#echo 'have any particular version of laravel in mind'
+	#read version
+	while true; do
+    read -p "Do you have any particular version of laravel in mind(eg: 5.8.*)?" yn
+    case $yn in
+        [Yy]* ) echo -n "Provide the version"
+				read; 
+				composer create-project laravel/laravel="${REPLY}" $laravel_root_folder
+				break;;
+        [Nn]* ) composer create-project --prefer-dist laravel/laravel $laravel_root_folder;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+# end
